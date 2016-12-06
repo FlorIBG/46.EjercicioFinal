f7c2e56d4b3ca6d3ca27ce41b91332c7972d62fc
FlorIBG <florivonne.bg@gmail.com>
2016-12-06 11:13:05 -0600

    etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,10 +1,14 @@
 $(document).ready( function(){
     $(".js-menu").hide();
-    console.log("recipe.html listo")
+    console.log("la página está lista")
     $('.js-show-recipe').click(function(){
         $('.page').removeClass('make');
+        $("a.active").removeClass("active");
+     $(this).addClass("active");
     });
     $('.js-show-make').click(function(){
         $('.page').addClass('make');
+        $("a.active").removeClass("active");
+        $(this).addClass("active");
     });
 });

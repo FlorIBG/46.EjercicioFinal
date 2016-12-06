9ff3c51becaac28ca94896ec271848d1c6ab3f0b
FlorIBG <florivonne.bg@gmail.com>
2016-12-06 11:02:39 -0600

    etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,6 +1,10 @@
 $(document).ready( function(){
-
-$(".js-menu").hide();
-console.log("recipe.html listo")
-
+    $(".js-menu").hide();
+    console.log("recipe.html listo")
+    $('.js-show-recipe').click(function(){
+        $('.page').removeClass('make');
+    });
+    $('.js-show-make').click(function(){
+        $('.page').addClass('make');
+    });
 });
